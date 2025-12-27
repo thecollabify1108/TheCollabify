26c3114926230a2880db0042146cd5fc066fe7ed
Nitin <itsprince0719@gmail.com>
2025-12-27 12:10:36 +0530

    Update logo with new colorful star and link to home page

--- frontend/src/components/common/Navbar.jsx
+++ frontend/src/components/common/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = () => {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                     {/* Logo */}
-                    <Link to={getDashboardLink()} className="flex items-center space-x-3">
+                    <Link to="/" className="flex items-center space-x-3">
                         <img src="/star-logo.png" alt="" className="h-8 w-8 object-contain" />
                         <div className="flex flex-col">
                             <span className="text-lg font-bold text-dark-100">The Collabify.ai</span>
